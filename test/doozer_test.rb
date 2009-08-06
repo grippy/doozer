d17c8b18ee5c516b7dea110d2461f4ef466a0015
grippy <massgripnosis@hotmail.com>
2009-08-06 10:00:36 -0700

    Initial commit to doozer.

---
+++ test/doozer_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class DoozerTest < Test::Unit::TestCase
+  should "probably rename this file and start testing for real" do
+    flunk "hey buddy, you should probably rename this file and start testing for real"
+  end
+end
