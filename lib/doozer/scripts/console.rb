becc8736eb3d78e0a5f363382a5fccc4fa9475ba
grippy <gmelton@whorde.com>
2009-08-11 08:08:05 -0700

    Version bump to 0.1.3

---
+++ lib/doozer/scripts/console.rb
@@ -0,0 +1,15 @@
+# require 'date'
+# require 'rubygems'
+# require 'doozer'
+# # APP_PATH = Dir.pwd
+# # require File.join(DOOZER_PATH, 'doozer/scripts/console')
+# @env = (ARGV.length > 0) ? ARGV[0].to_sym : :development
+# p @env
+p 'hello'
+# require 'date'
+# require 'rubygems'
+# $:.unshift File.join(File.dirname(__FILE__), '..', 'lib')
+# require 'doozer'
+# require 'doozer/scripts/console'
+# Doozer::Initializer.console(*ARGV)
+p __FILE__
