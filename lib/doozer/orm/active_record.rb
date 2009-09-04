e56aa5a4e10c864076d2756b361c74e37a6eec04
grippy <gmelton@whorde.com>
2009-09-04 16:14:20 -0700

    remove allow_concurrency

--- lib/doozer/orm/active_record.rb
+++ lib/doozer/orm/active_record.rb
@@ -3,7 +3,7 @@ module Doozer
   module ORM
     def self.load
       db_config = Doozer::Configs.db()
-      ActiveRecord::Base.allow_concurrency = true
+      #ActiveRecord::Base.allow_concurrency = true
       ActiveRecord::Base.establish_connection(
         :adapter  => db_config["adapter"],
         :host     => db_config["host"],
@@ -13,7 +13,7 @@ module Doozer
       )
       printf "ORM: #{Doozer::Configs.orm()} initialized...\n"
       # printf "ORM: logging initialized"
-      ActiveRecord::Base.logger = Doozer::Configs.logger 
+      ActiveRecord::Base.logger = Doozer::Configs.logger
     end
   end
 end
