8e0b54766ea34800c03f464b31c43cfd0246bd66
grippy <gmelton@whorde.com>
2010-02-05 22:06:02 -0800

    added a model loading method for tasks

--- lib/doozer/task.rb
+++ lib/doozer/task.rb
@@ -4,6 +4,11 @@ module Doozer
     def initialize(args=nil)
       @args = args
     end
+    def models(list)
+      for m in list
+        require "app/models/#{m.to_s}"
+      end
+    end
     def name; return self.class.to_s end
     def help; end
     def run; end
